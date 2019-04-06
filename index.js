ea9a85dc267dfea278df54e4bf7b1e0fd5fa7104
Daniel Felizardo <daniel.david772@gmail.com>
2019-04-06 15:05:23 -0300

    tentando resolver erro admin

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const dbConnection = sqlite.open(path.resolve(__dirname, "banco.sqlite"), {
 const port = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "views"));
+app.set("admin", path.join(__dirname, "views/admin"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
