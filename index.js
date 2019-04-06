b8f5fa7afee27358f59eea90be44df369e648a3a
Daniel Felizardo <daniel.david772@gmail.com>
2019-04-06 14:16:27 -0300

    altering path for error

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const sqlite = require("sqlite");
-const dbConnection = sqlite.open(
-  path.resolve(__dirname, "banco.sqlite", { Promise })
-);
+const dbConnection = sqlite.open(path.resolve(__dirname, "banco.sqlite"), {
+  Promise
+});
 
 const port = process.env.PORT || 3000;
 
